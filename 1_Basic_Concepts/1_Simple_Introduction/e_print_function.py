dbbdde8d1cd671c9d3f0e1f0d72ac598f8fd60c5
Madhu Babu Kencha <madhukencha.programmer@gmail.com>
2021-07-02 17:38:57 +0530

    Changing naming convension

--- 1_Basic_Concepts/1_Simple_Introduction/e_print_function.py
+++ 1_Basic_Concepts/1_Simple_Introduction/e_print_function.py
@@ -9,7 +9,7 @@ Syntax
 ------
 print(value1, ..., sep=" ", end="\n", file=sys.stdout, flush=False/True)
 
-NOTE: you study more about it in build in functions section
+NOTE: you study more about it in built-in functions section
 """
 print("Total:", 20)
 print("Total value find below:\n", 20)
