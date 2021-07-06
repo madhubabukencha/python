de634b451c47640320d5f01d18fa666a4bbc0ab2
Madhu Babu Kencha <madhukencha.programmer@gmail.com>
2021-07-06 22:22:10 +0530

    Adding content which deleted by mistakenly

---
+++ 1_Basic_Concepts/3_operators/a_basic_operators.py
@@ -0,0 +1,23 @@
+"""
+Name           : Madhu Babu Kencha
+created on     : 13-Sep-2018
+Last Modified  : 04-Jul-2021
+The operators +, -, * and / work just like in most other languages.
+parentheses (()) can be used for grouping.
+"""
+
+
+# The "+" operator returns the sum of two operands
+print("2+5=%d" % (2+5))
+# The "-" operator returns the subtraction of two operands
+print("2-5=%d" % (2-5))
+# It will print the multiplication
+print("2*5=%d" % (2*5))
+# The "/" operator returns the quotient
+print("5/2=%f" % (5/2))
+# The "//" also returns the quotient but without fraction
+print("5//2=", 5//2)
+# It will return remainder of two operands
+print("5%2=", (5 % 2))
+# It will prints both quotient and remainder
+print(divmod(5, 3))
