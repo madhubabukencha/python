78284a6db535072ec75d756f257ea81d0927bac5
Madhu Babu Kencha <madhukencha.programmer@gmail.com>
2021-07-11 23:06:24 +0530

    Adding comparison and identity operators
    Change-Id: Iaa56ed10acfe67d9b2fbd0a46a3d8b281038ca03

---
+++ 1_Basic_Concepts/3_operators/c_assignment_operators.py
@@ -0,0 +1,49 @@
+"""
+Name           : Madhu Babu Kencha
+created on     : 13-Sep-2018
+Last Modified  : 11-Jul-2021
+
+Assignment operators are used to assign a value to variable.
+Assignment operators we can be use with combination of both
+arithmetic and bitwise operators
+"""
+# pylint: disable= C0103
+
+# Equal sing(=),assigns right value to the left  side variable
+RelationShip = "Amma Nanna"
+print(RelationShip)
+
+# +=,increment the left side variable based on right side value
+age = 10
+age += 12
+print(age)
+
+# -=,decrement the left side variable based on right side value
+age = 10
+age -= 2
+print(age)
+
+# *=,multiples the left side variable value with right side value
+age = 10
+age *= 2
+print(age)
+
+# /=,divides the left side variable value with right side value
+age = 10
+age /= 3
+print(age)
+
+# //=,Floor division happens on the left side variable value with right side value
+age = 10
+age //= 2
+print(age)
+
+# %=,Gives the remainder of left side variable based on right side value
+age = 10
+age %= 2
+print(age)
+
+# **=, Gives the exponent of left side variable based on right side value
+age = 10
+age **= 3
+print(age)
