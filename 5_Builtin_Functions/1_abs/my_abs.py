0eb00b3257f93ae95f6cd1d5c45e58b93a54d79e
Madhu Babu Kencha <madhukencha.programmer@gmail.com>
2021-08-09 21:09:45 +0530

    Build-in-func: Adding example for abs function
    Change-Id: Icaf270134492c850c5e84eb2353bb60dca563073

---
+++ 5_Builtin_Functions/1_abs/my_abs.py
@@ -0,0 +1,24 @@
+"""
+Name    :Madhu Babu Kencha
+Date    :04-April-2020
+
+In a single word absolute means non negative value
+"""
+# pylint: disable=C0103
+
+# Absolute value of an Integer
+temperature = -20
+print(f"Absolute value of int {temperature}: {abs(temperature)}")
+
+# Absolute value of a Float
+y_axis = 54.3321
+print(f"Absolute value of float {y_axis}: {abs(y_axis)}")
+
+# Absolute value of a complex number
+# In case complex numbers, abs() returns it's magnitude
+# magnitude of 3-4j ==> |3-4j|^2 = 3^2 + 4^2
+#                   ==> |3-4j| = sqrt(9+16)
+#                   ==> |3-4j| = 5.0
+
+complex_number = 3-4j
+print(f"Absolute value of complex number {complex_number}: {abs(complex_number)}")
