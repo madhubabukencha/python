4848c76ef7b7e94a059b70eaaa5c8971e759b2a8
Madhu Babu Kencha <madhukencha.programmer@gmail.com>
2023-12-21 22:17:58 +0530

    Adding pytest mocking

--- 6_Python_Standard_Library/Third_Party_libraries/pytest_demo/source/c_service.py
+++ 6_Python_Standard_Library/Third_Party_libraries/pytest_demo/source/c_service.py
@@ -1,3 +1,5 @@
+import requests
+
 # Creating a synthetic database, in the real world this might real database
 database = {
     1: "Madhu",
@@ -8,3 +10,11 @@ database = {
 
 def get_user_from_db(user_id):
     return database.get(user_id)
+
+
+def get_user():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise requests.exceptions.HTTPError(f"HTTPError: {response.status_code}")
--- 6_Python_Standard_Library/Third_Party_libraries/pytest_demo/test/test_service.py
+++ 6_Python_Standard_Library/Third_Party_libraries/pytest_demo/test/test_service.py
@@ -1,4 +1,6 @@
 import pytest
+import requests
+
 # proper import of the package matters a lot, wasted 2 days for it.
 from ..source import c_service
 import unittest.mock as mock
@@ -12,3 +14,40 @@ def test_get_user_from_db(mock_get_user_from_db):
     mock_get_user_from_db.return_value = "Mocked Madhu"
     user_name = c_service.get_user_from_db(1)
     assert user_name == "Mocked Madhu"
+
+
+@mock.patch("requests.get")
+def test_get_user(mock_get):
+    """
+    Testing API success scenario
+    """
+    # Creates a mock object to simulate the response object returned
+    # by the requests.get method.
+    mock_response = mock.Mock()
+    # Sets the status code of the mock response to 200,
+    # indicating a successful HTTP response.
+    mock_response.status_code = 200
+    #  Sets the return value of the json method of the mock response to a
+    #  dictionary, simulating the JSON data returned by the API.
+    mock_response.json.return_value = {"id": 1, "name": "Madhu"}
+    # print(f"\nmock_response: {mock_response}")
+    # Configures the mock requests.get method to return the mock
+    # response when called.
+    mock_get.return_value = mock_response
+    # Calls the get_user function from your module, which internally
+    # calls requests.get, but in this test, it gets the mocked response.
+    data = c_service.get_user()
+    # print(f"\nData: {data}")
+    assert data == {"id": 1, "name": "Madhu"}
+
+
+@mock.patch("requests.get")
+def test_get_user_error(mock_get):
+    """
+    Testing the failure scenario
+    """
+    mock_response = mock.Mock()
+    mock_response.status_code = 400
+    mock_get.return_value = mock_response
+    with pytest.raises(requests.exceptions.HTTPError):
+        c_service.get_user()
