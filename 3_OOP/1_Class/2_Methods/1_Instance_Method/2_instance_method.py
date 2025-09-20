6923413f717aa00a57a7d34dfedfaf7fed282f67
Madhu Babu Kencha <madhukencha.programmer@gmail.com>
2025-09-20 19:05:31 +0530

    Updating instance method details

--- 3_OOP/1_Class/2_Methods/1_Instance_Method/2_instance_method.py
+++ 3_OOP/1_Class/2_Methods/1_Instance_Method/2_instance_method.py
@@ -1,6 +1,8 @@
 """
 Name : Madhu Babu Kencha
-Date : 22-Jun-2022
+Created : 22-Jun-2022
+Last update: 20-Sep-2025
+L
 
 In this program we will learn that for instance methods
 it's not possible to change class state.
@@ -8,18 +10,28 @@ it's not possible to change class state.
 
 
 class Demo:
+    """
+    This class how to deal with class and instance variables
+    and how to change them.
+    """
     country = "India"
 
     def __init__(self):
+        """
+        You don't even need instance methods until and unless you
+        want to deal with instance variables.
+        """
         pass
 
     def display(self):
         print(self.country)
         # It is specific to object
-        self.country = "Chain"
+        self.country = "China"
+
         # If you enable it, it will changes class states
         # So, that all object value changes
         # Demo.country = "China"
+
         print(self.country)
 
 
@@ -27,9 +39,14 @@ object_1 = Demo()
 object_1.display()
 object_1.country = "America"
 print(object_1.country)
-print("\n")
+
+print("-----Creating new object-----")
+# This shows that class state is not changed
 object_2 = Demo()
 object_2.display()
-print("\n")
+
+print("-----Changing class state-----")
+# To change class state
+Demo.country = "Russia"
 object_3 = Demo()
 object_3.display()
--- 3_OOP/1_Class/2_Methods/1_Instance_Method/3_instance_method.py
+++ 3_OOP/1_Class/2_Methods/1_Instance_Method/3_instance_method.py
@@ -1,12 +1,17 @@
 """
 Name : Madhu Babu Kencha
 Date : 15-08-2023
+Last update: 20-Sep-2025
 
 What will happen if class attributes and instance variables have same name?
 """
 
 
 class Train:
+    """
+    This class shows what will happen if class attributes and
+    instance variables have same name.
+    """
     train = "Bharat Express"
 
     def __init__(self):
@@ -22,8 +27,8 @@ hyderabad = Train()
 # If you are using instance then you are only able to access instance variables
 # if comment __init__ method it will also points to class attribute
 print(hyderabad.train)
+# Since we used __str__ method it will print instance variable
+print(hyderabad)
 
 # To access class variable from instance
 print(hyderabad.__class__.train)
-
-print(hyderabad)
